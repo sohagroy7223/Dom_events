706bdc71798c549abdab9470f4136082d6de8f16
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-15 20:55:12 +0600

    addad click the button and change the element valu

---
+++ js/handle.js
@@ -0,0 +1,6 @@
+const title= document.getElementById("btn-click")
+// console.log(title)    
+.addEventListener("click", function () {
+    const headingText = document.getElementById("heading-text")
+    headingText.innerText="this is change sohag, by the button"
+})
