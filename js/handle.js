d46caee105c290f344991725d162cd85dbfe49cc
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-15 21:38:09 +0600

    addad an input field , p and button if text the input and preass the button input text auto set in p

--- js/handle.js
+++ js/handle.js
@@ -4,3 +4,9 @@ const title= document.getElementById("btn-click")
     const headingText = document.getElementById("heading-text")
     headingText.innerText="this is change sohag, by the button"
 })
+
+ const login = document.getElementById("log-in")
+       .addEventListener("click", function (){
+        const user = document.getElementById("user-info")
+        user.innerText="sohag"
+       })
