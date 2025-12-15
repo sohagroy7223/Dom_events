e6b61f165249f5ec7954242b2b89000ae239450e
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-15 22:49:47 +0600

    solve the all problems

--- js/handle.js
+++ js/handle.js
@@ -6,7 +6,19 @@ const title= document.getElementById("btn-click")
 })
 
  const login = document.getElementById("log-in")
-       .addEventListener("click", function (){
-        const user = document.getElementById("user-info")
-        user.innerText="sohag"
-       })
+ .addEventListener("click", function (){
+  const user = document.getElementById("user-info")
+  user.innerText="sohag"
+})
+
+const btnUpdate = document.getElementById("update-btn")
+// console.log(btnUpdate)
+.addEventListener("click", function (){
+    // get the text input field 
+    const inputName  = document.getElementById("input-name")
+    const name = inputName.value;
+    // console.log(name)
+    // set the name
+    const nameP = document.getElementById("name")
+    nameP.innerText = name
+})
