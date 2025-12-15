ee13f17cfa7c17c6d1e12e989a5b5ca0e234377a
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-15 13:52:41 +0600

    addad body color change in many away in this file

--- js/events.js
+++ js/events.js
@@ -1,1 +1,27 @@
-console.log("events is worked")
+// option 2 to handle event 
+
+function makeBlue (){
+    document.body.style.background = "blue"
+}   
+
+function makeRed(){
+    document.body.style.background="red"
+}
+
+// option 3 get element by id and then set onclick
+
+const makeGreen = document.getElementById("make-green")
+// console.log(makeGreen)
+makeGreen.onclick = function makeGreen (){
+    document.body.style.backgroundColor = "green"
+}
+
+// option 3 different version 
+
+const makePurple = document.getElementById("btn-make-purple")
+// console.log(makePurple)
+   makePurple.onclick = parple;
+   function parple (){
+      document.body.style.backgroundColor = "purple"
+ }
+
